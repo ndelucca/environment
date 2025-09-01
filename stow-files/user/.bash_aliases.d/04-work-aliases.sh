9ed41125b7c07683a3968f1a1d5f96f0390d4c4b
ndelucca <ndelucca@educativa.com>
2025-09-01 11:27:15 -0300

    add some mounting for work

--- stow-files/user/.bash_aliases.d/04-work-aliases.sh
+++ stow-files/user/.bash_aliases.d/04-work-aliases.sh
@@ -4,3 +4,7 @@ DEBIAN="deb12"
 alias vm="cd ~/vagrant/vm/${DEBIAN}"
 alias vup=". ~/vagrant/vm/${DEBIAN}/.envrc && vm && vagrant up"
 alias vhalt=". ~/vagrant/vm/${DEBIAN}/.envrc && vm && vagrant halt"
+
+alias vmmount="sshfs educativa@${VM_HOSTNAME}: /home/ndelucca/vmmount/educativa"
+alias vmumount="fusermount -u /home/ndelucca/vmmount/educativa"
+
