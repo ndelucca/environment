28c37234c73692d46e50c40ea88a10e99c659abd
ndelucca <nazadelucca@gmail.com>
2025-10-25 16:29:36 -0300

    xdg-portals in wayland

--- stow-files-wayland/user/.bash_aliases.d/00-aliases.sh
+++ stow-files-wayland/user/.bash_aliases.d/00-aliases.sh
@@ -5,3 +5,5 @@ alias ap='ansible-playbook'
 
 alias myip='printf "external: " && curl ifconfig.me && echo && printf "local: " && hostname -I'
 
+alias google-chrome="google-chrome --disable-background-networking --ozone-platform=wayland --enable-features=UseOzonePlatform"
+alias chromium="chromium --disable-background-networking --ozone-platform=wayland --enable-features=UseOzonePlatform"
