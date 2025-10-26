66f5eb98a4d12dae6fc4f8452d6e2489ddf2b0ae
ndelucca <nazadelucca@gmail.com>
2025-10-26 19:49:59 -0300

    chromium and google-chrome aliases for wayland

--- stow-files-wayland/user/.bash_aliases.d/00-aliases.sh
+++ stow-files-wayland/user/.bash_aliases.d/00-aliases.sh
@@ -5,5 +5,10 @@ alias ap='ansible-playbook'
 
 alias myip='printf "external: " && curl ifconfig.me && echo && printf "local: " && hostname -I'
 
-alias google-chrome="google-chrome --disable-background-networking --ozone-platform=wayland --enable-features=UseOzonePlatform"
-alias chromium="chromium --disable-background-networking --ozone-platform=wayland --enable-features=UseOzonePlatform"
+alias google-chrome="/usr/bin/google-chrome --use-gl=angle --use-angle=gles-egl --disable-background-networking --ozone-platform=wayland --enable-wayland-ime --use-cmd-decoder=passthrough --ignore-gpu-blocklist --ignore-gpu-blacklist --enable-accelerated-video-decode --enable-features=VaapiVideoDecoder,VaapiVideoEncoder --gtk-version=4 --disable-gpu-memory-buffer-video-frames"
+alias chromium="/usr/bin/chromium --use-gl=angle --use-angle=gles-egl --disable-background-networking --ozone-platform=wayland --enable-wayland-ime --use-cmd-decoder=passthrough --ignore-gpu-blocklist --ignore-gpu-blacklist --enable-accelerated-video-decode --enable-features=VaapiVideoDecoder,VaapiVideoEncoder --gtk-version=4 --disable-gpu-memory-buffer-video-frames"
+
+# (gl=egl-angle,angle=opengl)
+# (gl=egl-angle,angle=opengles)
+# (gl=egl-angle,angle=vulkan)
+# (gl=egl-angle,angle=swiftshader)
