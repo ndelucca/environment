c44f6abadc2d37e5e95927b515f2a3da7d5f8c9d
ndelucca <ndelucca@protonmail.com>
2025-11-04 00:21:11 -0300

    completed git setup

--- fedora-sway-spin/setup/00-git-bash.sh
+++ fedora-sway-spin/setup/00-git-bash.sh
@@ -5,6 +5,10 @@ set -euo pipefail
 sudo git config --system user.name "ndelucca"
 sudo git config --system user.email "ndelucca@protonmail.com"
 sudo git config --system pull.rebase true
+sudo git config --system push.default simple
+sudo git config --system core.autocrlf false
+sudo git config --system core.commentchar ";"
+sudo git config --system color.ui true
 sudo git config --system alias.st status
 
 cat > "${HOME}/.bash_aliases" <<EOF
