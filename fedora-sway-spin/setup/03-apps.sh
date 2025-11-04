d801859489e9d2c8da4568985c4cd680c7fcfcf1
ndelucca <ndelucca@protonmail.com>
2025-11-04 00:15:22 -0300

    initial neovim setup

--- fedora-sway-spin/setup/03-apps.sh
+++ fedora-sway-spin/setup/03-apps.sh
@@ -10,3 +10,16 @@ sudo dnf install -y \
     chromium \
     neovim python3-neovim
 
+echo "Setting up Neovim configuration..."
+CONFIG_DIR="${HOME}/.config/nvim"
+REPO="ssh://git@github.com/ndelucca/nvim.git"
+
+mkdir -p "$(dirname "$CONFIG_DIR")"
+
+if [ -d "$CONFIG_DIR" ]; then
+    cd "$CONFIG_DIR"
+    git pull
+else
+    git clone "$REPO" "$CONFIG_DIR"
+fi
+
