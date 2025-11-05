c55302e43025adcd09b0ad6f7e8807af0f8f212f
ndelucca <ndelucca@protonmail.com>
2025-11-04 22:26:11 -0300

    webapps configuration for dmenu

--- fedora-sway-spin/dotfiles/.bashrc.d/00-aliases.sh
+++ fedora-sway-spin/dotfiles/.bashrc.d/00-aliases.sh
@@ -1,14 +1,6 @@
 alias ll='ls -l'
 alias la='ls -A'
 alias l='ls -CF'
-alias ap='ansible-playbook'
 
 alias myip='printf "external: " && curl ifconfig.me && echo && printf "local: " && hostname -I'
 
-alias google-chrome="/usr/bin/google-chrome --use-gl=angle --use-angle=gles-egl --disable-background-networking --ozone-platform=wayland --enable-wayland-ime --use-cmd-decoder=passthrough --ignore-gpu-blocklist --ignore-gpu-blacklist --enable-accelerated-video-decode --enable-features=VaapiVideoDecoder,VaapiVideoEncoder --gtk-version=4 --disable-gpu-memory-buffer-video-frames"
-alias chromium="/usr/bin/chromium --use-gl=angle --use-angle=gles-egl --disable-background-networking --ozone-platform=wayland --enable-wayland-ime --use-cmd-decoder=passthrough --ignore-gpu-blocklist --ignore-gpu-blacklist --enable-accelerated-video-decode --enable-features=VaapiVideoDecoder,VaapiVideoEncoder --gtk-version=4 --disable-gpu-memory-buffer-video-frames"
-
-# (gl=egl-angle,angle=opengl)
-# (gl=egl-angle,angle=opengles)
-# (gl=egl-angle,angle=vulkan)
-# (gl=egl-angle,angle=swiftshader)
---
+++ fedora-sway-spin/setup/04-webapps.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+WEBAPPS_DIR="$HOME/.local/share/applications/webapps"
+mkdir -p "$WEBAPPS_DIR"
+
+declare -A WEBAPPS=(
+    [Spotify]="https://open.spotify.com"
+    [Tidal]="https://listen.tidal.com"
+    [Teams]="https://teams.microsoft.com"
+    [ChatGPT]="https://chat.openai.com"
+    [Discord]="https://discord.com/app"
+    [WhatsApp]="https://web.whatsapp.com"
+)
+
+CHROMIUM_BIN="${CHROMIUM_BIN:-chromium-browser}"
+DEFAULT_ICON="chromium"
+
+create_webapp() {
+    local name="$1"
+    local url="$2"
+    local desktop_file="$WEBAPPS_DIR/${name}.desktop"
+
+    echo "Creating or updating: $name"
+    tee "$desktop_file" >/dev/null <<EOF
+[Desktop Entry]
+Name=${name}
+Exec=${CHROMIUM_BIN} --app="${url}" --class=${name}
+Icon=${DEFAULT_ICON}
+Type=Application
+Categories=WebApps;
+EOF
+}
+
+for app in "${!WEBAPPS[@]}"; do
+    create_webapp "$app" "${WEBAPPS[$app]}"
+done
+
+update-desktop-database "$WEBAPPS_DIR" >/dev/null 2>&1 || true
