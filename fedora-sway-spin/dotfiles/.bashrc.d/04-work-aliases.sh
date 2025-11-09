5dfa033b71e91b3133d39a1edaabf709f0afd052
ndelucca <ndelucca@protonmail.com>
2025-11-09 15:53:31 -0300

    change vm work files

--- fedora-sway-spin/dotfiles/.bashrc.d/04-work-aliases.sh
+++ fedora-sway-spin/dotfiles/.bashrc.d/04-work-aliases.sh
@@ -1,9 +1,9 @@
 VM_HOSTNAME="nazareno-vm.e-ducativa.x"
 DEBIAN="deb12"
 
-alias vm="cd ~/vagrant/vm/${DEBIAN}"
-alias vup=". ~/vagrant/vm/${DEBIAN}/.envrc && vm && vagrant up"
-alias vhalt=". ~/vagrant/vm/${DEBIAN}/.envrc && vm && vagrant halt"
+alias vm="cd ~/deploy/vm/${DEBIAN}"
+alias vup=". ~/deploy/vm/${DEBIAN}/.envrc && vm && vagrant up"
+alias vhalt=". ~/deploy/vm/${DEBIAN}/.envrc && vm && vagrant halt"
 
 alias vmmount="sshfs educativa@${VM_HOSTNAME}: /home/ndelucca/vmmount/educativa"
 alias vmumount="fusermount -u /home/ndelucca/vmmount/educativa"
