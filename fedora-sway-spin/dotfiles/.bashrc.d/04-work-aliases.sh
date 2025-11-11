8daf230c3096995e09eed60f8f657b88a03a3d40
ndelucca <ndelucca@protonmail.com>
2025-11-10 21:55:40 -0300

    add work alias

--- fedora-sway-spin/dotfiles/.bashrc.d/04-work-aliases.sh
+++ fedora-sway-spin/dotfiles/.bashrc.d/04-work-aliases.sh
@@ -1,10 +1,11 @@
 VM_HOSTNAME="nazareno-vm.e-ducativa.x"
 DEBIAN="deb12"
+MOUNTPOINT="/home/ndelucca/vmmount/educativa"
 
 alias vm="cd ~/deploy/vm/${DEBIAN}"
 alias vup=". ~/deploy/vm/${DEBIAN}/.envrc && vm && vagrant up"
-alias vhalt=". ~/deploy/vm/${DEBIAN}/.envrc && vm && vagrant halt"
+alias vhalt=". ~/deploy/vm/${DEBIAN}/.envrc && vm && vagrant halt && nmcli connection down educativa"
 
-alias vmmount="sshfs educativa@${VM_HOSTNAME}: /home/ndelucca/vmmount/educativa"
+alias vmmount="mkdir -p ${MOUNTPOINT} && sshfs educativa@${VM_HOSTNAME}: ${MOUNTPOINT}"
 alias vmumount="fusermount -u /home/ndelucca/vmmount/educativa"
 
