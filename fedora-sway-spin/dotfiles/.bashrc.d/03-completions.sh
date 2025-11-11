ba4c783db0aad4457e68e4fb9034a5ec6a8138f1
ndelucca <ndelucca@protonmail.com>
2025-11-11 17:26:32 -0300

    add sway-output-fix tool

--- fedora-sway-spin/dotfiles/.bashrc.d/03-completions.sh
+++ fedora-sway-spin/dotfiles/.bashrc.d/03-completions.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/env bash
 
-[[ -f /usr/share/bash-completion/bash_completion ]] &&
-    source /usr/share/bash-completion/bash_completion
+[[ -f /usr/share/bash-completion/bash_completion ]] && source /usr/share/bash-completion/bash_completion
 
-[[ -f /usr/share/bash-completion/completions/git ]] &&
-    source /usr/share/bash-completion/completions/git
+[[ -f /usr/share/bash-completion/completions/git ]] && source /usr/share/bash-completion/completions/git
 
-
-# Run direnv hooks
 eval "$(direnv hook bash)"
 
 /usr/bin/fortune | /usr/bin/cowsay -pn
